f36cd965c8246d8a5b8971e03ef28f23e24ce3ce
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-07-01 12:33:48 +0900

    Initial commit.

---
+++ mecab.d
@@ -0,0 +1,60 @@
+module mecab;
+
+import std.string;
+
+import mecab_c;
+
+
+alias MECAB_NOR_NODE NOR_NODE;
+alias MECAB_UNK_NODE UNK_NODE;
+alias MECAB_BOS_NODE BOS_NODE;
+alias MECAB_EOS_NODE EOS_NODE;
+alias MECAB_EON_NODE EON_NODE;
+
+struct Node {
+    private mecab_node_t* node_;
+
+    @property const(char)[] surface() pure { return node_.surface[0..node_.length]; }
+    @property const(char)[] feature() pure { return "";/*node_.feature;*/ }
+    @property uint   id()        pure { return node_.id; }
+    @property ushort length()    pure { return node_.length; }
+    @property ushort rlength()   pure { return node_.rlength; }
+    @property ushort rcAttr()    pure { return node_.rcAttr; }
+    @property ushort lcAttr()    pure { return node_.lcAttr; }
+    @property ushort posid()     pure { return node_.posid; }
+    @property ubyte  char_type() pure { return node_.char_type; }
+    @property ubyte  stat()      pure { return node_.stat; }
+    @property ubyte  isbest()    pure { return node_.isbest; }
+    @property float  alpha()     pure { return node_.alpha; }
+    @property float  beta()      pure { return node_.beta; }
+    @property float  prob()      pure { return node_.prob; }
+    @property short  wcost()     pure { return node_.wcost; }
+    @property long   cost()      pure { return node_.cost; }
+}
+
+struct ParsedNodes {
+    private mecab_t* mecab_;
+    private mecab_node_t* node_;
+    private const(char)* str_;
+
+    this(string str) {
+        str_ = str.toStringz;
+        mecab_ = mecab_new(0, null);
+        node_ = mecab_sparse_tonode(mecab_, str_);
+    }
+
+    @property bool empty() const { return !node_; }
+    Range opSlice() { return Range(node_); }
+    @property Node front() { return Node(node_); }
+
+    struct Range {
+        mecab_node_t* node_;
+        this(mecab_node_t* node) {
+            node_ = node;
+        }
+        @property bool empty() const { return !node_; }
+        @property Node front() { return Node(node_); }
+        void popFront() { node_ = node_.next; }
+        @property Range save() { return this; }
+    }
+}
---
+++ mecab_c.d
@@ -0,0 +1,52 @@
+module mecab_c;
+
+
+struct mecab_t;
+
+struct mecab_path_t {
+    mecab_node_t*  rnode;
+    mecab_path_t*  rnext;
+    mecab_node_t*  lnode;
+    mecab_path_t*  lnext;
+    int            cost;
+    float          prob;
+}
+
+struct mecab_node_t {
+    mecab_node_t*  prev;
+    mecab_node_t*  next;
+    mecab_node_t*  enext;
+    mecab_node_t*  bnext;
+    mecab_path_t*  rpath;
+    mecab_path_t*  lpath;
+    const(char)*   surface;
+    const(char)*   feature;
+    uint           id;
+    ushort         length;
+    ushort         rlength;
+    ushort         rcAttr;
+    ushort         lcAttr;
+    ushort         posid;
+    ubyte          char_type;
+    ubyte          stat;
+    ubyte          isbest;
+    float          alpha;
+    float          beta;
+    float          prob;
+    short          wcost;
+    long           cost;
+}
+
+enum {
+    MECAB_NOR_NODE = 0,
+    MECAB_UNK_NODE = 1,
+    MECAB_BOS_NODE = 2,
+    MECAB_EOS_NODE = 3,
+    MECAB_EON_NODE = 4
+};
+
+extern(C) {
+    mecab_t* mecab_new(int argc, char** argv);
+    mecab_node_t* mecab_sparse_tonode(mecab_t* mecab, const(char)* str);
+    mecab_node_t* mecab_sparse_tonode2(mecab_t* mecab, const(char)* str, size_t len);
+}
